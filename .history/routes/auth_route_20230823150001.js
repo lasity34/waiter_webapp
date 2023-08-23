839942eea6c2d8773ad5c80073f45e3898f8be2e
Bjorn Worrall <85297118+lasity34@users.noreply.github.com>
2023-08-23 22:18:41 +0200

    as

---
+++ .history/routes/auth_route_20230823150001.js
@@ -0,0 +1,38 @@
+export default function authRoute(adminService, waiterService) {
+  async function login(req, res) {
+    const { username, password } = req.body;
+
+    // Verify credentials in the admin table
+    const admin = await adminService.verifyCredentials(username, password);
+    if (admin) {
+      req.session.adminUsername = username;
+      return res.redirect(`/admin/${username}`);
+    }
+
+    // Verify credentials in the waiters table
+    const waiter = await waiterService.verifyCredentials(username, password);
+    if (waiter) {
+      return res.redirect(`/waiters/${username}`);
+    }
+
+    // Render the login page with an error message if credentials are not valid
+    res.render('login', { error: 'Invalid credentials' });
+  }
+
+
+  async function logout(req, res) {
+    // Destroy the session
+    req.session.destroy(err => {
+      if (err) {
+        return res.redirect('/error'); // Redirect to error page if something goes wrong
+      }
+      res.clearCookie('connect.sid'); // Clear the session cookie
+      res.redirect('/'); // Redirect to home or login page
+    });
+  }
+
+  return {
+    login,
+    logout
+  };
+}
---
+++ .history/services/admin_services_20230823221838.js
@@ -0,0 +1,96 @@
+import bcrypt from 'bcrypt'
+
+
+
+export default function adminService(db) {
+    
+        async function getAdminByUsername(username) {
+            return await db.oneOrNone('SELECT * FROM waiters_schedule.admins WHERE username = $1', [username])
+        }
+
+
+        async function verifyPassword(password, hash) {
+            return new Promise((resolve, reject) => {
+                bcrypt.compare(password, hash, function(err, result) {
+                    if (err) reject(err);
+                    resolve(result)
+                })
+            })
+        }
+
+        
+
+        async function createUser(username, password) {
+
+            const existingUser = await db.oneOrNone('SELECT * FROM waiters WHERE username = $1', [username]);
+
+            if (existingUser) {
+              throw new Error('User with this username already exists');
+            }
+          
+
+            const hashedPassword = await bcrypt.hash(password, 10);
+            return await db.oneOrNone(
+              `INSERT INTO waiters (username, password)
+               VALUES ($1, $2)
+               RETURNING *;`,
+              [username, hashedPassword]
+            );
+          }
+          
+        
+
+          async function listWaiters() {
+            return await db.any('SELECT username FROM waiters');
+          }
+
+          async function deleteUser(username) {
+            return await db.result('DELETE FROM waiters WHERE username = $1', [username])
+              .then(result => result.rowCount > 0)
+              .catch(error => {
+                console.error(error);
+                return false;
+              });
+          }
+
+          async function verifyCredentials(username, password) {
+            const admin = await getAdminByUsername(username);
+            if (admin) {
+              const isPasswordValid = await verifyPassword(password, admin.password);
+              if (isPasswordValid) {
+                return admin;
+              }
+            }
+            return null;
+          }
+
+          async function getShifts() {
+            return await db.any('SELECT day, time_slot, username FROM waiters_schedule');
+          }
+
+          async function deleteWaiterSchedule(username) {
+            await db.none('DELETE FROM waiters_schedule WHERE username = $1', [username]);
+          }
+
+
+
+        return {
+            getShifts,
+            getAdminByUsername,
+            verifyPassword,
+            createUser,
+            listWaiters,
+            deleteUser,
+            verifyCredentials,
+            deleteWaiterSchedule
+        }
+
+
+}
+
+
+
+
+
+
+
