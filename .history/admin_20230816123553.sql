a50659f38bd1d981237bc6cf156e2f219c1423b5
Bjorn Worrall <85297118+lasity34@users.noreply.github.com>
2023-09-04 10:55:53 +0200

    api

--- .history/admin_20230816123553.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE waiters_schedule.admins (
-    username VARCHAR(50) PRIMARY KEY,
-    password VARCHAR(50)
-);
-
-
-INSERT INTO waiters_schedule.admins (username, password) VALUES ('lyla', 'lyla123');
--- .history/routes/create_user_route_20230821092803.js
+++
@@ -1,9 +0,0 @@
-
-
-export default function create_user_route() {
-
-
-    
-
-
-}
